74a7e6487e5a6bfdf3567a63b1e9a848c87ddd84
patrickcardoso-dev <dev.patrickcardoso@gmail.com>
2024-12-14 17:04:10 -0300

    feat: add topbar component

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next';
+import { Public_Sans } from 'next/font/google';
+
+import TopBar from 'src/components/TopBar';
+
+import './globals.css';
+
+const publicSans = Public_Sans({
+  weight: ['400', '600', '700'],
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Seu Aeroporto',
+  description: 'Seu sistema unificado de serviços',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={publicSans.className}>
+        <TopBar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+import './styles.css';
+
+export default function TopBar() {
+  return (
+    <header className="header-area">
+      <div className="container flex">
+        <Image src="/images/airport-logo.jpg" alt="" width="160" height="44" />
+
+        <Image src="/images/operator-logo.jpg" alt="" width="44" height="44" />
+      </div>
+    </header>
+  );
+}
