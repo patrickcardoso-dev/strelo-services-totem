a4a477fcb4e1ad8fd988688fc2974a123d308588
patrickcardoso-dev <dev.patrickcardoso@gmail.com>
2024-12-17 19:23:00 -0300

    feat: add chat bot page

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -6,5 +6,18 @@ import './styles.css';
 
 export default async function Chatbot() {
   const menuItems = await getMenuItems();
-  return <NavigationHeader menuItems={menuItems} />;
+  return (
+    <>
+      <NavigationHeader menuItems={menuItems} />
+      <div className="container iframe-area">
+        <iframe
+          src="https://salvador-airport.strelo.com.br/"
+          title="Atendente Virtual"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
 }
